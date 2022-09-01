1f02a8d01417b0409528a4786ad28df6811cd7ec
Savanna Lee <77945964+savannalee@users.noreply.github.com>
2022-09-01 00:48:48 -0700

    Add files via upload
    initial commit

---
+++ MadLibsProject/src/madLibs.java
@@ -0,0 +1,197 @@
+//Savanna Lee
+//Period 3
+
+import java.util.*;
+
+public class madLibs {
+    public static void main(String[] args) {
+
+        Scanner a = new Scanner(System.in);
+
+        System.out.println("Enter a place starting with a capital letter: ");
+        String starshallow = a.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adj = a.nextLine();
+
+        System.out.println("Enter a girl's name: ");
+        String rory = a.nextLine();
+
+        System.out.println("Enter that girl's last name: ");
+        String gilmore = a.nextLine();
+
+        System.out.println("Enter a boy's name: ");
+        String dean = a.nextLine();
+
+        System.out.println("Enter that guy's last name: ");
+        String forester = a.nextLine();
+
+        System.out.println("Enter a verb in past tense: ");
+        String verbed1 = a.nextLine();
+
+        System.out.println("Enter a verb in past tense: ");
+        String verbed2 = a.nextLine();
+
+        System.out.println("Enter a verb ending in -ing: ");
+        String verbing1 = a.nextLine();
+
+        System.out.println("Enter something the boy has: ");
+        String noun1 = a.nextLine();
+
+        System.out.println("Enter a verb: ");
+        String verbpres1 = a.nextLine();
+
+        System.out.println("Enter a verb in past tense: ");
+        String verbed3 = a.nextLine();
+
+        System.out.println("Enter a school year (freshman, sophomore, junior, senior): ");
+        String schoolyear = a.nextLine();
+
+        System.out.println("Enter the name of an MSJ teacher: ");
+        String teacher1 = a.nextLine();
+
+        System.out.println("Enter the name of another MSJ teacher: ");
+        String teacher2 = a.nextLine();
+
+        System.out.println("Enter the name of your friend that goes to MSJ: ");
+        String friend1 = a.nextLine();
+
+        System.out.println("Enter the name of a month: ");
+        String month = a.nextLine();
+
+        System.out.println("Enter a year: ");
+        int year = a.nextInt();
+
+        a.nextLine();
+
+        System.out.println("Enter a plural measurement of time: ");
+        String years = a.nextLine();
+
+        System.out.println("Enter verb in past tense that you can do on someone: ");
+        String verb4 = a.nextLine();
+
+        System.out.println("Enter another boy's name: ");
+        String jess = a.nextLine();
+
+        System.out.println("Enter the boy's last name: ");
+        String mariano = a.nextLine();
+
+        System.out.println("Enter a plural noun: ");
+        String nouns1 = a.nextLine();
+
+        System.out.println("Enter a verb: ");
+        String verb2 = a.nextLine();
+
+        System.out.println("Enter a verb: ");
+        String verb3 = a.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adj1 = a.nextLine();
+
+        System.out.println("Enter a place: ");
+        String place1 = a.nextLine();
+
+        System.out.println("Enter a verb in past tense: ");
+        String verbed5 = a.nextLine();
+
+        System.out.println("Enter the name of a vehicle: ");
+        String vehicle = a.nextLine();
+
+        System.out.println("Enter the name of a city: ");
+        String city = a.nextLine();
+
+        System.out.println("Enter a university (don't include the word \"university\": )");
+        String university = a.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adj2 = a.nextLine();
+
+        System.out.println("Enter a gpa: ");
+        double gpa = a.nextDouble();
+
+        System.out.println("Enter an SAT score: ");
+        int sat = a.nextInt();
+
+        a.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adjective = a.nextLine();
+
+        System.out.println("Enter a plural measurement of time: ");
+        String timemeasureplu2 = a.nextLine();
+
+        System.out.println("Enter another boy's name: ");
+        String logan = a.nextLine();
+
+        System.out.println("Enter that boy's last name: ");
+        String huntzberger = a.nextLine();
+
+        System.out.println("Enter a holiday or special day: ");
+        String specialday = a.nextLine();
+
+
+        System.out.println("Enter another girl's name (first and last): ");
+        String odette = a.nextLine();
+
+        System.out.println("Enter a number: ");
+        int number = a.nextInt();
+
+        a.nextLine();
+
+        System.out.println("Enter a plural measurement of time: ");
+        String yes = a.nextLine();
+
+        System.out.println("Enter a plural measurement of time: ");
+        String timemeasureplu4 = a.nextLine();
+
+        System.out.println("Enter an occupation: ");
+        String occupation = a.nextLine();
+
+        System.out.println("Enter the name of the original girl's best friend: ");
+        String paris = a.nextLine();
+
+        System.out.println("Enter the girl's last name: ");
+        String geller = a.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adj4 = a.nextLine();
+
+        System.out.println("Enter an adjective: ");
+        String adj5 = a.nextLine();
+
+        System.out.println("Enter a number bigger than 1: ");
+        int numberbigthan1 = a.nextInt();
+
+        System.out.println("Once upon a time in a land known as " + starshallow + "land, there was a " + adj + " girl");
+        System.out.println("named " + rory + " " + gilmore +  " who really, really liked this guy named " + dean + " " + forester + ".");
+        System.out.println(dean + " liked " + rory + " for the way she " + verbed1 + " and the way she ");
+        System.out.println(verbed2 + "." + " Even when she was " + verbing1 + ", something " + dean + " hated, he still");
+        System.out.println("liked her. " + rory + " liked " + dean + "'s " + noun1 + " and how he used it to " + verbpres1 + ".");
+        System.out.println("She also just liked the way he " + verbed3 + " at her. They started dating during ");
+        System.out.println(schoolyear + " year and flaunted their relationship to people like " + teacher1 + ", ");
+        System.out.println(teacher2 + ", " + friend1 + ", and others. All was well until " + month + " of ");
+        System.out.println(year + ". After just a " + years + " of dating, " + dean + " told " + rory + " that");
+        System.out.println("he " + verb4 + " her. " + rory + " reacted in a state of confusion. " + dean + " didn't take that " );
+        System.out.println("well and left " + rory + " in the dust. Eventually, " + rory + " got over him and found ");
+        System.out.println("a new guy named " + jess + " " + mariano + ". " + jess + " was not like the other " + nouns1 + ". He ");
+        System.out.println("loved to " + verb2 + ", just like " + rory + ". He loved to " + verb3 + ", just like ");
+        System.out.println(rory + ". It was a match made in " + adj1 + " " + place1 + ". One day, out of no where, " + jess);
+        System.out.println(verbed5 + " off on a " + vehicle + " and headed for " + city + ". " + rory + " was devastated");
+        System.out.println("and swore off men for eternity. " + "But of course, she could not get ");
+        System.out.println("enough of the  men here at " + starshallow + "land. When she went off to ");
+        System.out.println("college at " + university + " University with her " + gpa + " gpa and " + sat + " SAT score, she ");
+        System.out.println("attracted many " + adj2 + " and " + adjective + " men. In just a few " + timemeasureplu2 + ", she");
+        System.out.println("began dating the one and only " + logan + " " + huntzberger + ". They dated until " + rory);
+        System.out.println("graduated from " + university + " University. On " + specialday + " Day of the same year, ");
+        System.out.println(logan + "proposed to " + rory + ", but she rejected him. " + logan + " stormed off and");
+        System.out.println("this " + adj2 + " and " + adjective + " young fellow married French Heiress " + odette + ". ");
+        System.out.println("After " + number + " whole " + yes + " of dating, " + rory + " was still alone. After giving ");
+        System.out.println("up on dating" + " for " + number + " " + timemeasureplu4 + " "  + rory + " realized that this");
+        System.out.println("whole time, she was in love with her best " + occupation + " " + paris + " " + geller + ".");
+        System.out.println("After " + rory + " confessed her " + adj4 + " love for the " + adj5 + " " + paris + ", they");
+        System.out.println("got married and had " + numberbigthan1 + " kids together. They lived happily ever after.");
+        System.out.println("The end. #LiveLaughLoveLesbians");
+
+
+    }
+}
